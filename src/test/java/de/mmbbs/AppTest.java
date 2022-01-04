ed0152037372685220f828849baf85e3be4bcee5
jtuttas <jtuttas@gmx.net>
2022-01-04 09:29:39 +0100

    1 eq 1

---
+++ src/test/java/de/mmbbs/AppTest.java
@@ -0,0 +1,15 @@
+package de.mmbbs;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+
+public class AppTest {
+        
+
+    @Test
+    public void example() {
+        assertEquals(1, 1);
+    }
+}
+
+

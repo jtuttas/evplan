28417b8b19293c95de903cf8dfa45dd3f913624f
jtuttas <jtuttas@gmx.net>
2022-01-08 09:53:37 +0100

    getBeitrag

---
+++ docs/getBeitrag.java
@@ -0,0 +1,16 @@
+private double getBeitrag(int alter, int mitgliedsjahre) {
+    double beitrag = 0;
+    if (alter < 18) {
+        beitrag = 30.0;
+    } else if (alter <= 12) {
+        beitrag = 15.0;
+    } else {
+        beitrag = 50.0;
+    }
+    if (mitgliedsjahre > 5 && mitgliedsjahre <= 15) {
+        beitrag = beitrag * 0.9;
+    } else if (mitgliedsjahre > 15) {
+        beitrag = beitrag * 0.8;
+    }
+    return beitrag;
+}
